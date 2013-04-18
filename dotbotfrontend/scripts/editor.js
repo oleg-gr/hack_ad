398fe9e0d0566ae90704bfeb7daa1f64b143bd85
Moiri Gamboni <moiri@moiri-campuslife.(none)>
2013-04-19 01:28:09 +0400

    Creating js structure

--- dotbotfrontend/scripts/editor.js
+++ dotbotfrontend/scripts/editor.js
@@ -1,16 +1,15 @@
 /*javascript editor behaviour*/
-
-$(document).ready(function () {
-
-	var textArea = document.getElementById("editor");
-
-	var tnt = CodeMirror.fromTextArea(textArea, {
-		mode: "ruby",
-		indentUnit: 3,
-		tabSize: 3,
-		lineNumbers: true
-	});
-
-	tnt.setValue("#Program\ndefine functionName(x,y) do\n\tfd(20)\n\tx=x+y\nend");	
-	
-});
+var editor =
+{
+	init: function()
+	{
+		var textArea = document.getElementById("editor");
+		editor.doc = CodeMirror.fromTextArea(textArea, {
+			mode: "ruby",
+			indentUnit: 3,
+			tabSize: 3,
+			lineNumbers: true
+			});
+		editor.doc.setValue("#Program\ndefine functionName(x,y) do\n\tfd(20)\n\tx=x+y\nend");
+	}
+}
