02a2afbce75a6200c45e19583427240fb6907aed
lingz <lz781@nyu.edu>
2013-04-20 18:24:19 +0400

    renamed files

---
+++ dotbotserver/routes/pages.js
@@ -0,0 +1,15 @@
+exports.get = function(req, res){
+  res.render('info_' + req.params.page + '.html.ejs');
+};
+
+exports.index = function(req, res){
+  res.render('info.html.ejs');
+};
+
+exports.use = function(req, res){
+  res.render('use_robo.html.ejs');
+};
+
+exports.select = function(req, res){
+  res.render('use_select.html.ejs');
+};
