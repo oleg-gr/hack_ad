964eee1514184e4e7284278878bb8ca34a7cb35d
Oleg Grishin <og402@nyu.edu>
2013-04-21 02:40:13 +0400

    highlighting ore keywords

--- dotbotserver/public/javascripts/tnt.js
+++ dotbotserver/public/javascripts/tnt.js
@@ -5,7 +5,7 @@ CodeMirror.defineMode("tnt", function(config) {
     return o;
   }
   var keywords = wordObj(["and", "begin", "define", "do", "else",
-    "else if","end", "false", "if", "or", "true", "while", "print", "fd"
+    "else if","end", "false", "if", "or", "true", "while", "print", "fd","forward","lt","left","rt", "right", "bd", "backword"
   ]);
   var indentWords = wordObj(["do"]);
   var dedentWords = wordObj(["end", "else"]);
