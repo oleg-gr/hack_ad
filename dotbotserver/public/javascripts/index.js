227aeb9d37b21532ba7003ea4f085ee2d37665ba
lingz <lz781@nyu.edu>
2013-04-24 12:49:53 +0400

    cleanup

--- dotbotserver/public/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#navbar ul li a').bind('click', function(e) {
-   e.preventDefault();
-   $('html, body').animate({ scrollTop: $(this.hash).offset().top - 60 }, 300);
-  });
-});
-
