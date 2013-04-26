96e512a73d8f40ded4880e264535ab6c241bff09
Oleg Grishin <og402@nyu.edu>
2013-04-26 18:17:17 +0400

    Fixed codemirror autofocus
    There was a typo in codemirror instantiating
    Fixed default program

--- dotbotserver_node/public/javascripts/editor.js
+++ dotbotserver_node/public/javascripts/editor.js
@@ -9,30 +9,11 @@ var editor =
 			indentUnit: 3,
 			tabSize: 3,
 			lineNumbers: true,
-			autofocuse: true,
+			autofocus: true,
 			highlightSelectionMatches: true,
 			styleActiveLine: true
 			});
 			
-		this.doc.setValue("#Program\ndefine patrol(x) do\n\tforward(x)\n\tlt(180)\n\tforward(x)\n\trt(180)\nend");
+		this.doc.setValue("#Program\ndefine patrol(x) do\n\tforward(x)\n\tlt(180)\n\tforward(x)\n\trt(180)\nend\n\n#Patroling, 10cm\npatrol(10)");
 	},
-/*		
-	find:function() {
-	
-		this.doc.cursor = this.doc.getSearchCursor($("#search").val())
-	
-	},
-	
-	findn:function() {
-	
-		this.doc.cursor.findNext();
-	
-	},
-	
-	findp:function () {
-	
-		this.doc.cursor.findPrevious();
-
-	}
-*/
 }
